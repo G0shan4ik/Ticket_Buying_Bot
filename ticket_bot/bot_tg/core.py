472402304ee4f035c6c6f2b2cab568305d62a6cd
Egor <egorkononec64@gmail.com>
2024-12-02 17:59:55 +0300

    [-] create function check_pars_event

--- ticket_bot/bot_tg/core.py
+++ ticket_bot/bot_tg/core.py
@@ -2,6 +2,9 @@ from aiogram import Bot, Dispatcher, Router
 from aiogram.client.bot import DefaultBotProperties
 from aiogram.enums import ParseMode
 
+from ticket_bot.bot_tg.helpers import read_data_from_json
+from ticket_bot.parsers.parser import BuyingTicketsNikulina
+
 from os import getenv
 
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -25,6 +28,31 @@ router = Router()
 dp.include_router(router)
 
 
+async def check_pars_event() -> None:
+    """
+        The function that runs all 'active' events from the users_filters
+        file is launched at program startup and runs once.
+
+    :return: None
+    """
+    all_data = await read_data_from_json()
+    if all_data:
+        for item in all_data:
+            for user, data in item.items():
+                if data[-1] == 'active':
+                    start = BuyingTicketsNikulina(
+                        event_filter='; '.join(data[0]),
+                        all_user_data={
+                            'user_id': int(user),
+                            'data': {f"{user}": data}
+                        },
+                        bot=bot_
+                    )
+                    await start.run_parser()
+    return
+
+
 async def start_bot() -> None:
     await bot_(DeleteWebhook(drop_pending_updates=True))
-    await asyncio.gather(dp.start_polling(bot_))
+
+    await asyncio.gather(dp.start_polling(bot_), check_pars_event())
